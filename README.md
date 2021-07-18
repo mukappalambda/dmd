2c361ee83508baf9564bb0a8215a15e5f12827f9
mukappalambda <maokailan24@gmail.com>
2021-07-18 15:02:36 +0800

    tweak example02 and the HackMD note

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Python Implementation of Dynamic Mode Decomposition
 
-This notebook illustrates how to apply Dynamic Mode Decomposition (DMD) to univariate time series forecasting tasks.
+This python code illustrates how to apply Dynamic Mode Decomposition (DMD) to univariate time series forecasting tasks.
 
-Two examples are provided here,
+Two examples are provided here.
 
 For the explanation of DMD, please refer to this HackMD notes: [Dynamic Mode Decomposition](https://hackmd.io/@mklan/HyLXh7UH_).
 
 The major reference is this arXiv article: [On Dynamic Mode Decomposition: Theory and Applications](https://arxiv.org/abs/1312.0041).
+
+---
+
+To experiment with the examples, run
+
+```bash
+pip install -r requirements.txt
+
+python dmd_example01.py
+python dmd_example02.py
+```
+
+## History
+
+- Tweak the HackMD note more readable and add the Python code inside that note.
---
+++ dmd_example02.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from dmd import DMD
+
+plt.style.use("ggplot")
+
+def read_temperature(only_returns_temp: bool = True) -> pd.DataFrame:
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv"
+    df_raw = pd.read_csv(url)
+
+    if only_returns_temp:
+        return df_raw[["Temp"]]
+
+    return df_raw
+
+def fit_dmd(ts_length: int) -> DMD:
+    dmd = DMD()
+    dmd.fit(df, ts_length=ts_length)
+    return dmd
+
+
+def make_pred_from_dmd(dmd: DMD, ts_length: int, df: pd.DataFrame, t: int):
+    pred_x = np.arange(t, t+ts_length)
+    pred_y = dmd.predict_future(t)
+    return pred_x, pred_y
+
+def main_plot(df: pd.DataFrame, fname: str):
+    ts_length = 1500
+    dmd = fit_dmd(ts_length)
+    pred_x0, pred_y0 = make_pred_from_dmd(dmd, ts_length, df, 0)
+    pred_x1, pred_y1 = make_pred_from_dmd(dmd, ts_length, df, df.shape[0])
+
+    figsize = (24, 10)
+    plt.clf()
+    plt.figure(figsize=figsize)
+
+    plt.plot(df, "r", label="raw data")
+    plt.plot(pred_x0, pred_y0, "b", label="fitted data")
+    plt.plot(pred_x1, pred_y1, "g", label="future prediction")
+    plt.legend()
+
+    plt.savefig(fname)
+
+if __name__ == "__main__":
+    df = read_temperature()
+    fname = "temperature-data.png"
+    main_plot(df=df, fname=fname)
